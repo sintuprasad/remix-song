11a125f9ec271df94f0920a4f7ebd78e81c35c65
sintuprasad <sintuprasad792@gmail.com>
2023-12-12 17:11:00 +0530

    remix website

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const alg = document.querySelector('.mp3_details');
+const title = alg.querySelector('#title');
+const poster = alg.querySelector('#poster');
+const album = alg.querySelector('#album');
+const singer = alg.querySelector('#singer');
+const remixer = alg.querySelector('#remixer');
+const size = alg.querySelector('#size');
+const duration = alg.querySelector('#duration');
+const time = alg.querySelector('#time');
+
+
+let ads = document.getElementById("#ads").innerHTML="hii";
+console.log("gfuyrtfuy");
+
+
